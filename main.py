9f4ab7123084c841e6112856bfa56f8a64d9cc80
James Simerson <jrsimerson@gmail.com>
2026-07-03 16:35:44 +0900

    Main Menu Print

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def main_menu():
+    """
+    Purpose: to display menu
+    """
+    print("====MAIN MENU====")
+    print("1. View Tasks")
+    print("2. Add Tasks")
+    print("3. Delete Tasks")
+    print("4. Exit")   
+    print("Choose an Option") 
+# end def
+    
+main_menu()

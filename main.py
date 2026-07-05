2140246dd8f09deaa9c6b3e64cf2ef2b89553509
James Simerson <jrsimerson@gmail.com>
2026-07-06 01:21:03 +0900

    updated tasks display function

--- main.py
+++ main.py
@@ -23,16 +23,21 @@ def main_menu():
         if user_input == "4":
             print("Exiting...")
             break
+        elif user_input == "1":
+            number = 1
+            if len(tasks) == 0:
+                print("no tasks found ")                   
+            else:
+                for number, task in enumerate(tasks, start=1):
+                    print(f"{number}. {task}")
         elif user_input == "2":
             task = input("what task would you like to add? ")
             tasks.append(task)
-            print(tasks)
-        elif user_input == "1":
-            print(tasks)
+            print("Task added! ")
         elif user_input == "3":
             delete = input("What task would you like to delete? ")
             tasks.remove(delete)
-            print(tasks)
+            print(f"=== ",delete, " was removed from your list. ===")
 
 
     

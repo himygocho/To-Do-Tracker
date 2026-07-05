cd23917ff0584c12585d243426c2722987c1e4b5
James Simerson <jrsimerson@gmail.com>
2026-07-05 20:35:21 +0900

    Main Menu Updated Loop

--- main.py
+++ main.py
@@ -3,12 +3,26 @@ def main_menu():
     """
     Purpose: to display menu
     """
-    print("====MAIN MENU====")
-    print("1. View Tasks")
-    print("2. Add Tasks")
-    print("3. Delete Tasks")
-    print("4. Exit")   
-    print("Choose an Option") 
-# end def
+    while True:
+
+        print("====MAIN MENU====")
+        print("1. View Tasks")
+        print("2. Add Tasks")
+        print("3. Delete Tasks")
+        print("4. Exit")   
+        print("Choose an Option") 
+
+        user_input = input()
+        if user_input == "4":
+            print("Exiting...")
+            break
+
+
+    
+
+        
+    
     
 main_menu()
+
+

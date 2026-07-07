4eedd121332ebc80319cf0295037c9bcc717cdde
James Simerson <jrsimerson@gmail.com>
2026-07-07 19:38:29 +0900

    Replaced HTML List with Python List

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+tasks = [
+    "Workout",
+    "Study Python",
+    "Read Bible",
+    "Run"
+]
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        tasks=tasks
+        )
 
 if __name__ == "__main__":
     app.run(debug=True)
+

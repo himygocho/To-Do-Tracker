da1cd3a23b2066bd1d7c90e2028918c16d88215d
James Simerson <jrsimerson@gmail.com>
2026-07-08 12:43:51 +0900

    Added Delete button

--- app.py
+++ app.py
@@ -16,7 +16,23 @@ def home():
 @app.route("/add", methods=["POST"])
 def add():
     task = request.form["task"]
-    tasks.append(task)
+
+    new_task = {
+        "id": len(tasks) + 1,
+        "name": task
+    }
+
+    tasks.append(new_task)
+    return redirect(url_for("home"))
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    task_id = int(request.form["task_id"])
+
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
